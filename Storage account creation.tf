9ed31c449354b1ded2fbe784811866e7a5988d7d
unknown <ravikumarsheokand1985@gmail.com>
2025-07-29 19:59:32 +0530

    update

---
+++ Storage account creation.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5c83ee41-460c-400b-8120-13a218b61103"
+}
+
+resource "azurerm_resource_group" "ravi" {
+  name     = "RKS_rg"
+  location = "West Europe"
+}
+
+# resource "azurerm_resource_group" "ravi1" {
+#   name     = "RKS_rg1"
+#   location = "Central India"
+# }
+
+# resource "azurerm_storage_account" "ravistg" {
+#   name                     = "rksstorage"
+#   resource_group_name      = "RKS_rg"
+#   location                 = "West Europe"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+# }
+
+# resource "azurerm_storage_account" "ravistg1" {
+#   name                     = "rksstorage1"
+#   resource_group_name      = "RKS_rg1"
+#   location                 = "Central India"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+# }
