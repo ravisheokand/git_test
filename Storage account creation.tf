7211eb554562cc940a810107a19ecb3c06dc63b7
unknown <ravikumarsheokand1985@gmail.com>
2025-07-29 21:35:48 +0530

    update

--- Storage account creation.tf
+++ Storage account creation.tf
@@ -17,23 +17,10 @@ resource "azurerm_resource_group" "ravi" {
   location = "West Europe"
 }
 
-# resource "azurerm_resource_group" "ravi1" {
-#   name     = "RKS_rg1"
-#   location = "Central India"
-# }
-
-# resource "azurerm_storage_account" "ravistg" {
-#   name                     = "rksstorage"
-#   resource_group_name      = "RKS_rg"
-#   location                 = "West Europe"
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-# }
-
-# resource "azurerm_storage_account" "ravistg1" {
-#   name                     = "rksstorage1"
-#   resource_group_name      = "RKS_rg1"
-#   location                 = "Central India"
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-# }
+resource "azurerm_storage_account" "ravistg" {
+  name                     = "rksstorage"
+  resource_group_name      = "RKS_rg"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
